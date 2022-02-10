71350f5408be1dbf2731b867056dfca18378ceb1
gamaliele <gamaliel.e.g.l@gmail.com>
2022-02-10 20:53:14 -0300

    Segundo Cambio

--- ejercicio4/src/main/MainR.java
+++ ejercicio4/src/main/MainR.java
@@ -14,7 +14,7 @@ import servicios.Rectangulos;
  */
 public class MainR {
 
-    /**
+    /**prueba dos
      * @param args the command line arguments
      */
     public static void main(String[] args) {
